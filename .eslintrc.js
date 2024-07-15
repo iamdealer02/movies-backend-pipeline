21b4be625ab9f5f2c53bf667ef93d95054a3d541
AmaySunilShenoy <amaysunilshenoy02@gmail.com>
2024-07-15 17:07:44 +0530

    Initial Commit with PR and Deploy Workflows and ts. eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+      project: 'tsconfig.json',
+    },
+    root: true,
+    env: {
+      node: true,
+      jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    plugins: ['@typescript-eslint'],
+    rules:{
+      "@typescript-eslint/interface-name-prefix":"off",
+      "@typescript-eslint/explicit-function-return-type":"error",
+      "@typescript-eslint/explicit-module-boundary-types":"off",
+      "@typescript-eslint/no-explicit-any":"warn",
+      "@typescript-eslint/no-unused-vars":"error",
+      "no-console":"warn",
+      "no-restricted-syntax":[
+         "error",
+         "ForInStatement",
+         "LabeledStatement",
+         "WithStatement"
+      ]
+   },
+  };
