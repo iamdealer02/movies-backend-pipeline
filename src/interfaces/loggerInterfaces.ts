3837e9af13e26eb924547a9a500d6841216996fc
AmaySunilShenoy <amaysunilshenoy02@gmail.com>
2024-07-15 20:12:46 +0530

    feat: winstionOptions Interface

---
+++ src/interfaces/loggerInterfaces.ts
@@ -0,0 +1,8 @@
+import winston from 'winston'
+
+interface winstonOptions extends winston.LoggerOptions {
+    file: winston.transports.FileTransportOptions;
+    console: winston.transports.ConsoleTransportOptions;
+  }
+
+export {winstonOptions}
