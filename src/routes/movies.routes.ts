42de6fcce6120f63aa083304df3a54184faa34cb
AmaySunilShenoy <amaysunilshenoy02@gmail.com>
2024-07-17 02:35:08 +0530

    feat: Added getTopMovies controller and /movies/top route

--- src/routes/movies.routes.ts
+++ src/routes/movies.routes.ts
@@ -4,5 +4,6 @@ import * as moviesController from '../controllers/movies.controller';
 const router = express.Router();
 
 router.get('/', moviesController.getMovies);
+router.get('/top', moviesController.getTopRatedMovies);
 
 export default router;
