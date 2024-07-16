daf379dd299c996eb6a0f7b438b74f3f870d0bb8
AmaySunilShenoy <amaysunilshenoy02@gmail.com>
2024-07-16 11:33:45 +0530

    feat: Added postgres db connection logic

---
+++ src/constants/statusCodes.ts
@@ -0,0 +1,17 @@
+const queryError: number = 500;
+const notFound: number = 404;
+const badGateway: number = 502;
+const success: number = 200;
+const unauthorized: number = 401;
+const badRequest: number = 400;
+const userAlreadyExists: number = 409;
+
+export {
+  queryError,
+  notFound,
+  badGateway,
+  success,
+  unauthorized,
+  badRequest,
+  userAlreadyExists,
+};
