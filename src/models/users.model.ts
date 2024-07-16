5f8f6268adb61073157283da2120a901d396d546
Upasana Sharma <sharmaupasana823@gmail.com>
2024-07-16 13:39:10 +0200

    feat: User Model + Interface Creation

---
+++ src/models/users.model.ts
@@ -0,0 +1,15 @@
+import {Schema, model} from "mongoose";
+import { IUser } from "src/interfaces/user.interface";
+
+const userSchema = new Schema<IUser>({
+
+    username: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    messages: [{type: Schema.Types.ObjectId, ref: 'Message'}],
+    createdAt: {type: Date, required: true},
+    updatedAt: {type: Date, required: true}
+
+});
+
+export const User = model<IUser>('User', userSchema);
