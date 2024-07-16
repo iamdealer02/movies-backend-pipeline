fb376555715c5e6a1ac675eeccb96322a55cc927
Upasana Sharma <sharmaupasana823@gmail.com>
2024-07-16 13:43:37 +0200

    feat: User model + User Interface - proper linting - interface creation with model

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,11 +1,11 @@
 import { Types } from 'mongoose';
 
 export interface IUser {
-    _id?: Types.ObjectId;
-    username: string;
-    email: string;
-    password: string;
-    messages: Types.ObjectId[];
-    createdAt: Date;
-    updatedAt: Date;
+  _id?: Types.ObjectId;
+  username: string;
+  email: string;
+  password: string;
+  messages: Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
 }
