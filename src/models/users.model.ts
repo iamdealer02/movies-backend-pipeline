22d871b8e2bd1eaf37082d53531fe05da4fc6c53
Upasana Sharma <sharmaupasana823@gmail.com>
2024-07-19 22:25:07 +0200

    fix: User Model Fix

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,13 +1,32 @@
 import { Schema, model } from 'mongoose';
 import { IUser } from 'src/interfaces/user.interface';
 
-const userSchema = new Schema<IUser>({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
-  createdAt: { type: Date, required: true },
-  updatedAt: { type: Date, required: true },
-});
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }], // Define `messages` as an array of ObjectId references
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+  },
+);
 
 export const User = model<IUser>('User', userSchema);
