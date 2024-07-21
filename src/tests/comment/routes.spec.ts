31b80ff257b84ee3f1ed9918d46bf92fd15ad6a1
AmaySunilShenoy <amaysunilshenoy02@gmail.com>
2024-07-21 14:41:18 +0530

    test: Added test for comment model, addComment controller and route

---
+++ src/tests/comment/routes.spec.ts
@@ -0,0 +1,82 @@
+import request from 'supertest';
+
+import { App } from 'supertest/types';
+import { Response, Request } from 'express';
+
+import * as commentController from '../../controllers/comment.controller';
+import { registerCoreMiddleWare } from '../../boot/setup';
+
+jest.mock('../../controllers/comment.controller');
+jest.mock('../../boot/database/db_connect', () => ({
+  query: jest.fn(),
+}));
+
+// To avoid log statements in the console during tests and to close logger stream (openHandle error)
+jest.mock('../../middleware/winston', () => ({
+  error: jest.fn(),
+  info: jest.fn(),
+  http: jest.fn(),
+}));
+
+describe('Testing comment routes', () => {
+  let app: App;
+  beforeAll(() => {
+    app = registerCoreMiddleWare();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('POST /comment/:movie_id', () => {
+    const addCommentMock = commentController.addComment as jest.Mock;
+
+    it('should create a comment for a given movie successfully', (done) => {
+      addCommentMock.mockImplementation(async (_req: Request, res: Response) =>
+        res.status(200).json({ message: 'Comment added' }),
+      );
+      request(app)
+        .post('/comments/12345')
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body).toEqual({ message: 'Comment added' });
+          expect(addCommentMock).toHaveBeenCalledTimes(1);
+          done(err);
+        });
+    });
+
+    it('should return 400 if movie_id is NaN missing or body is empty', (done) => {
+      addCommentMock.mockImplementation(async (_req: Request, res: Response) =>
+        res.status(400).json({ message: 'Missing parameters' }),
+      );
+
+      request(app)
+        .post(`/comments/nan`)
+        .expect(400)
+        .end((err, res) => {
+          expect(res.body).toEqual({ message: 'Missing parameters' });
+          expect(addCommentMock).toHaveBeenCalledTimes(1);
+          done(err);
+        });
+    });
+
+    it('should return 500 if query error occurs', (done) => {
+      addCommentMock.mockImplementation(async (_req: Request, res: Response) =>
+        res
+          .status(500)
+          .json({ error: 'Exception occured while adding comment' }),
+      );
+
+      request(app)
+        .post(`/comments/12345`)
+        .expect(500)
+        .end((err, res) => {
+          expect(res.body).toEqual({
+            error: 'Exception occured while adding comment',
+          });
+          expect(addCommentMock).toHaveBeenCalledTimes(1);
+          done(err);
+        });
+    });
+  });
+});
