44186f74cb84ae1d23f253f793471b92565f334f
AmaySunilShenoy <amaysunilshenoy02@gmail.com>
2024-07-21 14:39:49 +0530

    feat: Added addComment controller and '/comments/:movie_id' respective route

---
+++ src/controllers/comment.controller.ts
@@ -0,0 +1,58 @@
+import logger from '../middleware/winston';
+import * as statusCodes from '../constants/statusCodes';
+import Comment from '../models/comment.model';
+import { Response, Request } from 'express';
+import { HydratedDocument } from 'mongoose';
+import { IComment } from '../interfaces/comment.interface';
+
+const addComment = async (
+  req: Request & {
+    params: { movie_id?: string };
+    body: {
+      rating?: number;
+      username?: string;
+      comment?: string;
+      title?: string;
+    };
+  },
+  res: Response,
+): Promise<Response> => {
+  const { movie_id } = req.params;
+  const { rating, username, comment, title } = req.body;
+
+  const movieId: number = parseInt(movie_id);
+
+  if (
+    !movie_id ||
+    isNaN(movieId) ||
+    !rating ||
+    !username ||
+    !comment ||
+    !title
+  ) {
+    return res
+      .status(statusCodes.badRequest)
+      .json({ message: 'Missing parameters' });
+  } else {
+    try {
+      const commentObj: HydratedDocument<IComment> = new Comment({
+        movie_id: movieId,
+        rating,
+        username,
+        comment,
+        title,
+      });
+
+      await commentObj.save();
+
+      return res.status(statusCodes.success).json({ message: 'Comment added' });
+    } catch (error) {
+      logger.error(error.stack);
+      return res
+        .status(statusCodes.queryError)
+        .json({ error: 'Exception occurred while adding comment' });
+    }
+  }
+};
+
+export { addComment };
