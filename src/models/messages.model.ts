b96dbc115752ef287b04079dd681b3dfb9285ff3
Pyrees011 <raunak.bhansali17@gmail.com>
2024-07-25 12:53:28 +0200

    feat: add message

--- src/models/messages.model.ts
+++ src/models/messages.model.ts
@@ -2,23 +2,23 @@ import { Schema, model } from 'mongoose';
 import { IMessage } from 'src/interfaces/message.interface';
 
 const messageSchema = new Schema<IMessage>(
-    {
-        name: {
-            type: String,
-        },
-        user: {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
+  {
+    name: {
+      type: String,
     },
-    {
-        timestamps: {
-          createdAt: 'created_at',
-          updatedAt: 'updated_at',
-        },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
     },
-)
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+  },
+);
 
-const Message = model<IMessage>("Message", messageSchema);
+const Message = model<IMessage>('Message', messageSchema);
 
 export default Message;
