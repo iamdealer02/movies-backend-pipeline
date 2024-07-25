02571ffa727f69071ce778008692430a2ae62eb7
Pyrees011 <raunak.bhansali17@gmail.com>
2024-07-25 18:34:26 +0200

    test: message controller

--- src/interfaces/message.interface.ts
+++ src/interfaces/message.interface.ts
@@ -1,6 +1,7 @@
 import { Types } from 'mongoose';
 
 export interface IMessage {
+  _id: Types.ObjectId;
   name: string;
   user?: Types.ObjectId;
   createdAt: Date;
--- src/tests/message/model.spec.ts
+++ src/tests/message/model.spec.ts
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose';
-import { testData } from "./test.data";
-import { IMessage } from "../../interfaces/message.interface";
+import { testData } from './test.data';
+import { IMessage } from '../../interfaces/message.interface';
 import Message from '../../models/messages.model';
 
 describe('Message Model test', () => {
   const sampleMessageValue: {
-      name: IMessage['name'];
-      user?: IMessage['user'];
-      createdAt: IMessage['createdAt'];
-      updatedAt: IMessage['updatedAt'];
-  } = testData
+    name: IMessage['name'];
+    user?: IMessage['user'];
+    createdAt: IMessage['createdAt'];
+    updatedAt: IMessage['updatedAt'];
+  } = testData;
 
   // it should throw a validation error for missing fields
   it('should throw a validation error for missing name', () => {
@@ -20,17 +20,17 @@ describe('Message Model test', () => {
   });
 
   it('should throw a validation error for missing user', () => {
-    const message = new Message({ name: 'mock message name'});
+    const message = new Message({ name: 'mock message name' });
     const err = message.validateSync();
     expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
     expect(err?.errors).toHaveProperty('user');
-  })
+  });
 
   it('should add a message successfully with all required fields', () => {
-    const message = new Message(sampleMessageValue)
+    const message = new Message(sampleMessageValue);
     const err = message.validateSync();
     expect(err).toBeUndefined();
     expect(message).toHaveProperty('name', sampleMessageValue.name);
     expect(message).toHaveProperty('user', sampleMessageValue.user);
-  })
+  });
 });
