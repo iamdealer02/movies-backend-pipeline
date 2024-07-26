8c44bf9f60b5d513aea39d527bd2c5c2e66764d3
Pyrees011 <raunak.bhansali17@gmail.com>
2024-07-26 02:03:39 +0200

    feat: get message by id controller + route

--- src/routes/messages.routes.ts
+++ src/routes/messages.routes.ts
@@ -4,5 +4,6 @@ import * as messageController from '../controllers/messages.controller';
 const router = express.Router();
 
 router.post('/add/message', messageController.addMessage);
+router.get('/:messageId', messageController.getMessageById);
 
 export default router;
