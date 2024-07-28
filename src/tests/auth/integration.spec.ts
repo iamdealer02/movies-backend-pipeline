a9bff01fcbc70a6dff2f715855236547a0be8757
Upasana Sharma <sharmaupasana823@gmail.com>
2024-07-28 21:10:23 +0200

    fix: linting fixed

--- src/tests/auth/integration.spec.ts
+++ src/tests/auth/integration.spec.ts
@@ -14,88 +14,83 @@ process.env.JWT_SECRET = 'testsecret';
 
 // To avoid log statements in the console during tests and to close logger stream (openHandle error)
 jest.mock('../../middleware/winston', () => ({
-    error: jest.fn(),
-    info: jest.fn(),
-    http: jest.fn(),
-  }));
-  
-  beforeAll(async () => {
-    try {
-      mongodb = await MongoMemoryServer.create();
-      const uri = mongodb.getUri();
-      connectToMongoDB(uri);
-      app = registerCoreMiddleWare();
-    } catch (err) {
-      throw err;
-    }
-  });
-  
-  afterAll(async () => {
-    await pool.end();
+  error: jest.fn(),
+  info: jest.fn(),
+  http: jest.fn(),
+}));
+
+beforeAll(async () => {
+  try {
+    mongodb = await MongoMemoryServer.create();
+    const uri = mongodb.getUri();
+    connectToMongoDB(uri);
+    app = registerCoreMiddleWare();
+  } catch (err) {
+    throw err;
+  }
+});
+
+afterAll(async () => {
+  await pool.end();
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongodb.stop();
+});
+
+describe('User Registration and Login Integration Tests', () => {
+  const testUser = {
+    email: 'test@gmail.com',
+    username: 'testuser',
+    password: 'testpassword',
+  };
+  afterEach(async () => {
+    // delete mongodb collections
     await mongoose.connection.dropDatabase();
-    await mongoose.connection.close();
-    await mongodb.stop();
   });
 
-  describe('User Registration and Login Integration Tests', () => {
-    const testUser = {
-        email: 'test@gmail.com',
-        username: 'testuser',
-        password: 'testpassword',
-    };
-    afterEach(async () => {
-        // delete mongodb collections
-        await mongoose.connection.dropDatabase();
+  describe('User Registration', () => {
+    it('should register a new user', async () => {
+      const response = await request(app)
+        .post('/auth/signup')
+        .send(testUser)
+        .expect(201);
+      expect(response.body).toHaveProperty('_id');
+      expect(response.body).toHaveProperty('created_at');
+      expect(response.body).toHaveProperty('email', testUser.email);
+      expect(response.body).toHaveProperty('username', testUser.username);
+      expect(response.body).toHaveProperty('password');
+      // expect a hashed password
+      expect(response.body.password).not.toEqual(testUser.password);
     });
 
-    describe('User Registration', () => {
-        it('should register a new user', async () => {
-            const response = await request(app)
-                .post('/auth/signup')
-                .send(testUser)
-                .expect(201);
-            expect(response.body).toHaveProperty('_id');
-            expect(response.body).toHaveProperty('created_at');
-            expect(response.body).toHaveProperty('email', testUser.email);
-            expect(response.body).toHaveProperty('username', testUser.username);
-            expect(response.body).toHaveProperty('password');
-            // expect a hashed password
-            expect(response.body.password).not.toEqual(testUser.password);
-        });
-
-        it('should return 400 status code if email or password is missing', async () => {
-            const response = await request(app)
-                .post('/auth/signup')
-                .send({ })
-                .expect(400);
-            expect(response.body).toEqual({ error: 'missing information' });
-        });
-    });
-
-    describe('User Login', () => {
-        // Register a user before testing login
-        beforeEach(async () => {
-            await request(app)
-                .post('/auth/signup')
-                .send(testUser)
-                .expect(201);
-        });
-        it('should login a user', async () => {
-
-            const response = await request(app)
-                .post('/auth/login')
-                .send(testUser)
-                .expect(200);
-            expect(response.body).toHaveProperty('token');
-        });
-
-        it('should return 401 status code if email or password is incorrect', async () => {
-            const response = await request(app)
-                .post('/auth/login')
-                .send({ email: testUser.email, password: 'wrongpassword' })
-                .expect(400);
-            expect(response.body).toEqual({ error: "Email or password don't match"  });
-        });
+    it('should return 400 status code if email or password is missing', async () => {
+      const response = await request(app)
+        .post('/auth/signup')
+        .send({})
+        .expect(400);
+      expect(response.body).toEqual({ error: 'missing information' });
     });
   });
-  
+
+  describe('User Login', () => {
+    // Register a user before testing login
+    beforeEach(async () => {
+      await request(app).post('/auth/signup').send(testUser).expect(201);
+    });
+    it('should login a user', async () => {
+      const response = await request(app)
+        .post('/auth/login')
+        .send(testUser)
+        .expect(200);
+      expect(response.body).toHaveProperty('token');
+    });
+
+    it('should return 401 status code if email or password is incorrect', async () => {
+      const response = await request(app)
+        .post('/auth/login')
+        .send({ email: testUser.email, password: 'wrongpassword' })
+        .expect(400);
+      expect(response.body).toEqual({ error: "Email or password don't match" });
+    });
+  });
+});
